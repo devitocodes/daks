9158b9150a760589726a358d9d8e092e92c8abba
Navjot Kukreja <navjotk@gmail.com>
2020-06-23 22:40:45 +0100

    Flake on fwiio

--- fwiio.py
+++ fwiio.py
@@ -2,6 +2,7 @@ import io
 import h5py
 from azureio import blob_from_bytes, load_blob_to_hdf5
 
+
 def load_shot(num, container="shots"):
     filename = "shot_%d.h5" % num
 
@@ -11,19 +12,23 @@ def load_shot(num, container="shots"):
         dt = f['dt'][()]
     return data, src_coords, dt
 
+
 def save_shot(shot_id, data, src_coords, dt, container="shots"):
     bio = io.BytesIO()
     with h5py.File(bio, 'w') as f:
         f['data'] = data
         f['src_coords'] = src_coords
         f['dt'] = dt
-    
-    blob_from_bytes(Blob(container, "shot_%s.h5"%str(shot_id), bio.getvalue()))
+
+    blob_from_bytes(Blob(container, "shot_%s.h5" % str(shot_id),
+                    bio.getvalue()))
+
 
 def load_model(model_name, datakey):
     with load_blob_to_hdf5(Blob("models", model_name)) as f:
         data = f[datakey][()]
     return data
+
 
 class Blob():
     def __init__(self, container, filename):
