b4bf69b035e465be52ac57523ae4ba7d426a4a89
Navjot Kukreja <navjotk@gmail.com>
2020-07-01 17:08:20 +0100

    This version of FWI passes the gradient test but produces Nans

--- fwi.py
+++ fwi.py
@@ -9,12 +9,12 @@ from devito.logger import error
 from examples.seismic import Receiver, Model
 from examples.seismic.acoustic import AcousticWaveSolver
 
-from overthrust import overthrust_model_iso, create_geometry
+from overthrust import overthrust_model_iso, create_geometry, overthrust_solver_iso
 from functools import partial
 from scipy.optimize import minimize, Bounds
 from util import Profiler, clip_boundary_and_numpy, mat2vec, vec2mat, reinterpolate
 from dask_setup import setup_dask
-from fwiio import load_shot
+from fwiio import load_shot, Blob
 
 
 profiler = Profiler()
@@ -26,7 +26,7 @@ profiler = Profiler()
 @click.option("--final-solution-basename", default="fwi", help="Filename for the final solution")
 @click.option("--tn", default=4000, type=int, help="Number of timesteps to run")
 @click.option("--nshots", default=20, type=int, help="Number of shots (already decided when generating shots)")
-@click.option("--shots-container", default="shots", type=str, help="Name of container to read shots from")
+@click.option("--shots-container", default="shots-iso", type=str, help="Name of container to read shots from")
 @click.option("--so", default=6, type=int, help="Spatial discretisation order")
 @click.option("--nbl", default=40, type=int, help="Number of absorbing boundary layers to add to the model")
 @click.option("--kernel", default="OT2", help="Computation kernel to use (options: OT2, OT4)")
@@ -40,8 +40,8 @@ def run(initial_model_filename, final_solution_basename, tn, nshots, shots_conta
     dtype = np.float32
     model, geometry, bounds = initial_setup(initial_model_filename, tn, dtype, so, nbl)
 
-    solver_params = {'filename': initial_model_filename, 'tn': tn, 'space_order': so, 'dtype': dtype, 'datakey': 'm0',
-                     'nbl': nbl, 'origin': model.origin, 'spacing': model.spacing, 'shots_container': shots_container}
+    solver_params = {'h5_file': Blob("models", initial_model_filename), 'tn': tn, 'space_order': so, 'dtype': dtype, 'datakey': 'm0',
+                     'nbl': nbl, 'origin': np.array(model.origin), 'spacing': np.array(model.spacing), 'shots_container': shots_container}
 
     client = setup_dask()
 
@@ -115,56 +115,33 @@ def initial_setup(filename, tn, dtype, space_order, nbl, datakey="m0"):
 # This runs on the dask worker in the cloud.
 # Anything passed into or returned from this function will be serialised and sent over the network.
 def fwi_gradient_shot(vp_in, i, solver_params):
+    error("Starting eval")
+    shots_container = solver_params.pop("shots_container")
+    error("Loading shot")
+    rec, source_location, old_dt = load_shot(i, container=shots_container)
     error("Initialising solver")
-    tn = solver_params['tn']
-    nbl = solver_params['nbl']
-    space_order = solver_params['space_order']
-    dtype = solver_params['dtype']
-    origin = solver_params['origin']
-    spacing = solver_params['spacing']
-
-    shots_container = solver_params['shots_container']
-
-    true_d, source_location, old_dt = load_shot(i, container=shots_container)
-
-    model = Model(vp=vp_in, nbl=nbl, space_order=space_order, dtype=dtype, shape=vp_in.shape,
-                  origin=origin, spacing=spacing, bcs="damp")
-    geometry = create_geometry(model, tn, source_location)
-
-    error("tn: %d, nt: %d, dt: %f" % (geometry.tn, geometry.nt, geometry.dt))
-
-    error("Reinterpolate shot from %d samples to %d samples" % (true_d.shape[0], geometry.nt))
-    true_d = reinterpolate(true_d, geometry.nt, old_dt)
-
-    solver = AcousticWaveSolver(model, geometry, kernel='OT2', nbl=nbl,
-                                space_order=space_order, dtype=dtype)
-
-    grad = Function(name="grad", grid=model.grid)
-
-    residual = Receiver(name='rec', grid=model.grid,
-                        time_range=geometry.time_axis,
-                        coordinates=geometry.rec_positions)
-
-    u0 = TimeFunction(name='u', grid=model.grid, time_order=2, space_order=solver.space_order,
-                      save=geometry.nt)
-
+    solver = overthrust_solver_iso(**solver_params)
+    error("setting vp")
+    solver.model.update('vp', vp_in)
+    error("vp norm: %f, min: %f, max: %f" % (np.linalg.norm(solver.model.vp.data), np.min(solver.model.vp.data), np.max(solver.model.vp.data)))
     error("Forward prop")
-    smooth_d, _, _ = solver.forward(save=True, u=u0)
-    error("Misfit")
-    residual.data[:] = smooth_d.data[:] - true_d[:]
+    rec0, u0, _ = solver.forward(save=True)
+    error("vp norm: %f, rec0 norm: %f" % (np.linalg.norm(vp_in), np.linalg.norm(rec0.data)))
+    residual = Receiver(name='rec', grid=solver.model.grid, data=rec0.data - rec,
+                        time_range=solver.geometry.time_axis,
+                        coordinates=solver.geometry.rec_positions)
 
     objective = .5*np.linalg.norm(residual.data.ravel())**2
     error("Gradient")
-    solver.gradient(rec=residual, u=u0, grad=grad)
+    grad, _ = solver.gradient(residual, u0)
 
-    grad = clip_boundary_and_numpy(grad.data, model.nbl)
-
-    return objective, -grad
+    return objective, np.array(grad.data, dtype=solver_params['dtype'])
 
 
 def fwi_gradient(vp_in, model, geometry, nshots, client, solver_params):
+    print("vp", np.linalg.norm(vp_in), np.min(vp_in), np.max(vp_in))
     start_time = time.time()
-    vp_in = vec2mat(vp_in, model.shape)
+    vp_in = np.array(vec2mat(vp_in, model.shape), dtype=solver_params['dtype'])
     f_vp_in = client.scatter(vp_in)  # Dask enforces this for large arrays
     assert(model.shape == vp_in.shape)
 
@@ -174,29 +151,34 @@ def fwi_gradient(vp_in, model, geometry, nshots, client, solver_params):
         futures.append(client.submit(fwi_gradient_shot, f_vp_in, i, solver_params,
                                      resources={'tasks': 1}))  # Ensure one task per worker (to run two, tasks=0.5)
 
-    shape = model.shape
-
+    shape = model.vp.shape
+    wait(futures)
     def reduction(*args):
         grad = np.zeros(shape)  # Closured from above
         objective = 0.
 
         for a in args:
             o, g = a
+            print(o, np.linalg.norm(g))
             objective += o
             grad += g
         return objective, grad
 
-    reduce_future = client.submit(reduction, *futures)
-    wait(reduce_future)
+    #reduce_future = client.submit(reduction, *futures)
+    #wait(reduce_future)
 
-    objective, grad = reduce_future.result()
+    objective, grad = reduction(*[f.result() for f in futures])
+
+    #objective, grad = reduce_future.result()
     elapsed_time = time.time() - start_time
-    print("Objective function evaluation completed in %f seconds" % elapsed_time)
+    print("Objective function evaluation completed in %f seconds. F0=%f" % (elapsed_time, objective))
+
+    grad = clip_boundary_and_numpy(grad, model.nbl)
 
     # Scipy LBFGS misbehaves if type is not float64
     grad = mat2vec(np.array(grad)).astype(np.float64)
 
-    return objective, grad
+    return objective, -grad
 
 
 if __name__ == "__main__":
