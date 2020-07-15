8712222a59fcff7e79449098bb80e2175aaf035b
Navjot Kukreja <navjotk@gmail.com>
2020-07-15 09:58:24 +0100

    Checkpointing equivalence test that runs but fails

--- fwi-checkpointed.py
+++ fwi-checkpointed.py
@@ -1,20 +1,25 @@
 import numpy as np
 
 from fwi import initial_setup, fwi_gradient
-from fwiio import load_shot
-from util import mat2vec, vec2mat
+from fwiio import load_shot, Blob
+from util import mat2vec, vec2mat, clip_boundary_and_numpy
 from pyrevolve import Revolver
 from examples.checkpointing import CheckpointOperator, DevitoCheckpoint
 from devito import TimeFunction, Function
 from overthrust import overthrust_solver_iso
 from examples.seismic import Receiver
+from dask_setup import setup_dask
 
 
-def fwi_gradient_checkpointed(vp_in, model, geometry, nshots, client, solver_params, n_checkpoints=1000,
+def fwi_gradient_checkpointed(vp_in, nshots, client, solver, shots_container, scale_gradient=True,
+                              mute_water=True, exclude_boundaries=True, water_depth=20, n_checkpoints=1000,
                               compression_params=None):
     # Create symbols to hold the gradient and residual
-    grad = Function(name="grad", grid=model.grid)
-    vp = Function(name="vp", grid=model.grid)
+    model = solver.model
+    so = solver.space_order
+    geometry = solver.geometry
+    grad = Function(name="grad", grid=model.grid, space_order=so)
+    vp = Function(name="vp", grid=model.grid, space_order=so)
     smooth_d = Receiver(name='rec', grid=model.grid,
                         time_range=geometry.time_axis,
                         coordinates=geometry.rec_positions)
@@ -23,24 +28,25 @@ def fwi_gradient_checkpointed(vp_in, model, geometry, nshots, client, solver_par
                         coordinates=geometry.rec_positions)
     objective = 0.
     time_order = 2
-    vp_in = vec2mat(vp_in, model.shape)
 
-    assert(model.vp.shape == vp_in.shape)
-    vp.data[:] = vp_in[:]
-    filename = solver_params['filename']
+    if exclude_boundaries:
+        vp_in = np.array(vec2mat(vp_in, solver.model.shape), dtype=solver.model.dtype)
+    else:
+        vp_in = np.array(vec2mat(vp_in, solver.model.vp.shape), dtype=solver.model.dtype)
+    model.update("vp", vp_in)
+    vp.data[:] = model.vp.data[:]
 
-    solver = overthrust_solver_iso(filename, datakey="m0")
-    dt = solver.dt
+    dt = 1.75
     nt = smooth_d.data.shape[0] - 2
     u = TimeFunction(name='u', grid=model.grid, time_order=time_order,
-                     space_order=4)
+                     space_order=so)
     v = TimeFunction(name='v', grid=model.grid, time_order=time_order,
-                     space_order=4)
+                     space_order=so)
     fwd_op = solver.op_fwd(save=False)
     rev_op = solver.op_grad(save=False)
     cp = DevitoCheckpoint([u])
     for i in range(nshots):
-        true_d, source_location = load_shot(i)
+        true_d, source_location, old_dt = load_shot(i)
         # Update source location
         solver.geometry.src_positions[0, :] = source_location[:]
 
@@ -61,17 +67,59 @@ def fwi_gradient_checkpointed(vp_in, model, geometry, nshots, client, solver_par
         objective += .5*np.linalg.norm(residual.data.ravel())**2
         wrp.apply_reverse()
         print(wrp.profiler.summary())
+    if exclude_boundaries:
+        grad = clip_boundary_and_numpy(grad, solver.model.nbl)
 
+    if mute_water:
+        if exclude_boundaries:
+            muted_depth = water_depth
+        else:
+            muted_depth = water_depth + solver.model.nbl
+        grad[:, 0:muted_depth] = 0
+
+    if scale_gradient:
+        grad /= np.max(np.abs(grad))
     return objective, -np.ravel(grad.data).astype(np.float64)
 
 
 def verify_equivalence():
+    initial_model_filename = "overthrust_3D_initial_model_2D.h5"
+    tn = 4000
+    dtype = np.float32
+    so = 6
+    nbl = 40
+    exclude_boundaries = True
+    water_depth = 20
+    nshots = 2
+    client = setup_dask()
 
-    model, geometry, _ = initial_setup()
-    result1 = fwi_gradient_checkpointed(mat2vec(model.vp.data), model,
-                                        geometry)
+    shots_container = "shots-iso"
+    scale_gradient = True
+    mute_water = True
+    exclude_boundaries = True
 
-    result2 = fwi_gradient(mat2vec(model.vp.data), model, geometry)
+    solver_params = {'h5_file': Blob("models", initial_model_filename), 'tn': tn,
+                     'space_order': so, 'dtype': dtype, 'datakey': 'm0', 'nbl': nbl}
+
+    solver = overthrust_solver_iso(**solver_params)
+
+    model, geometry, _ = initial_setup(initial_model_filename, tn, dtype, so, nbl, datakey="m0",
+                                       exclude_boundaries=exclude_boundaries, water_depth=water_depth)
+
+    if exclude_boundaries:
+        v0 = mat2vec(clip_boundary_and_numpy(model.vp.data, model.nbl)).astype(np.float64)
+    else:
+        v0 = mat2vec(model.vp.data).astype(np.float64)
+
+    result1 = fwi_gradient_checkpointed(v0, nshots, client, solver, shots_container, scale_gradient, mute_water,
+                                        exclude_boundaries, water_depth)
+
+    result2 = fwi_gradient(v0, nshots, client, solver, shots_container, scale_gradient, mute_water, exclude_boundaries,
+                           water_depth)
 
     for r1, r2 in zip(result1, result2):
         np.testing.assert_allclose(r2, r1, rtol=0.01, atol=1e-8)
+
+
+if __name__ == "__main__":
+    verify_equivalence()
