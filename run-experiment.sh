42a8e266712bd90d22c2cc0c889570ae2a999cd7
Navjot Kukreja <navjotk@gmail.com>
2020-08-12 17:26:59 +0100

    Replace compression experiment with more generic shell script

--- run-experiment.sh
+++ run-experiment.sh
@@ -1,3 +1,10 @@
+VM_TYPE=$1
+shift
+EXPERIMENT_NAME=$1
+shift
+
+PARAMS="$@"
+
 exit_on_error() {
     exit_code=$1
     last_command=${@:2}
@@ -6,20 +13,21 @@ exit_on_error() {
         exit $exit_code
     fi
 }
-set -x
-RUN=$1
-VM_TYPE=$2
 
-CLUSTERNAME=compression$RUN
-DIRNAME=fwi_zfp$RUN
+RG_NAME=NAV_DAKS_$EXPERIMENT_NAME
+CLUSTERNAME=$EXPERIMENT_NAME
+DIRNAME=$EXPERIMENT_NAME
+LOCATION=eastus
 export DEVITO_LOGGING=ERROR
 
+echo Creating resource group
+az group create --name $RG_NAME --location $LOCATION
 echo Provisioning AKS...
-az aks create --resource-group Nav_DAKS1 --name $CLUSTERNAME --node-count 15 --generate-ssh-keys --attach-acr devitoaks --node-vm-size $VM_TYPE
+az aks create --resource-group $RG_NAME --name $CLUSTERNAME --node-count 15 --generate-ssh-keys --attach-acr devitoaks --node-vm-size $VM_TYPE
 
+exit_on_error $?
 
-
-az aks get-credentials --resource-group Nav_DAKS1 --name $CLUSTERNAME
+az aks get-credentials --resource-group $RG_NAME --name $CLUSTERNAME
 
 echo Deploying kubernetes configuration
 kubectl apply --context $CLUSTERNAME -f kubernetes/dask-cluster.yaml
@@ -52,11 +60,13 @@ do
     fi
 done
 sleep 30
-docker-compose run -e DASK_SERVER_IP=$DASK_IP -w /app/daks daks /venv/bin/python -u fwi/run.py --results-dir $DIRNAME --nshots 80 --shots-container shots-rho-80-so-8 --so 8 --scale-gradient L --checkpointing --compression zfp --tolerance $RUN --reference-solution fwi_referenceL/objective_function_values.csv 2>&1 | tee $DIRNAME/output.txt
+docker-compose run -e DASK_SERVER_IP=$DASK_IP -w /app/daks daks /venv/bin/python -u fwi/run.py --results-dir $DIRNAME $PARAMS 2>&1 | tee $DIRNAME/output.txt
 
 echo All done. Deleting resources
 
 kubectl config delete-context $CLUSTERNAME
 
-az aks delete --name $CLUSTERNAME --resource-group Nav_DAKS1 --yes
+az aks delete --name $CLUSTERNAME --resource-group $RG_NAME --yes --no-wait
 
+az group delete --name $RG_NAME --yes --no-wait
+az ad app list -o table | grep $CLUSTERNAME|  awk '{print "az ad app delete --id " $1}'|sh
