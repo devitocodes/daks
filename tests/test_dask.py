0a8ae06ee0b54e2d651e695e212ad9eddb7a2f49
Navjot Kukreja <navjotk@gmail.com>
2020-08-12 17:39:38 +0100

    Add minimal dask test

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 from distributed import wait
 
 from fwi.dasksetup import setup_dask
+from fwi.overthrust import overthrust_solver_iso
+from fwi.io import Blob
 
 
 def test_dask_upload():
@@ -18,3 +22,26 @@ def test_dask_upload():
     wait(future)
     result = future.result()
     assert(result)
+
+
+def test_remote_devito():
+    initial_model_filename = "overthrust_3D_initial_model_2D.h5"
+    tn = 4000
+    so = 6
+    dtype = np.float32
+    datakey = "m0"
+    nbl = 40
+    solver_params = {'h5_file': Blob("models", initial_model_filename), 'tn': tn,
+                     'space_order': so, 'dtype': dtype, 'datakey': datakey, 'nbl': nbl,
+                     'opt': ('noop', {'openmp': True, 'par-dynamic-work': 1000})}
+
+    solver = overthrust_solver_iso(**solver_params)
+
+    client = setup_dask()
+    future = client.submit(solver.forward)
+    rec1, u1, _ = solver.forward()
+    wait(future)
+    rec2, u2, _ = future.result()
+
+    assert((rec1.data == rec2.data).all())
+    assert((u1.data == u2.data).all())
