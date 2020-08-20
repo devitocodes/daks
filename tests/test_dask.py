fb94d637133d1c123b33572fec3558ec3b99841b
Navjot Kukreja <navjotk@gmail.com>
2020-08-20 15:55:26 +0100

    Different platforms can result in numerical mismatches beyond 1e-7

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -113,5 +113,5 @@ def test_remote_devito(solver, client):
     rec2, u2, _ = future.result()
     print(np.linalg.norm(rec1.data))
     print(np.linalg.norm(rec2.data))
-    assert(np.allclose(rec1.data, rec2.data, atol=0., rtol=0.))
-    assert(np.allclose(u1.data, u2.data, atol=0., rtol=0.))
+    assert(np.allclose(rec1.data, rec2.data, atol=1e-7, rtol=0.))
+    assert(np.allclose(u1.data, u2.data, atol=1e-7, rtol=0.))
