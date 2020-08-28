23351e36108bed4c666e66d41450df0b04a351f1
Navjot Kukreja <navjotk@gmail.com>
2020-08-28 12:43:55 +0100

    Report absolute value of error

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -117,7 +117,7 @@ def test_remote_devito(solver, client):
     error_rec = rec1.data - rec2.data
     rel_rec = error_rec/rec1.data
     print("rec", np.min(error_rec), np.max(error_rec), np.min(rel_rec), np.max(rel_rec))
-    error_u = u1.data - u2.data
+    error_u = np.absolute(u1.data - u2.data)
     rel_u = error_u/u1.data
     print("u", np.min(error_u), np.max(error_u), np.min(rel_u), np.max(rel_u))
     assert(np.allclose(rec1.data, rec2.data, atol=1e-7, rtol=1e-5))
